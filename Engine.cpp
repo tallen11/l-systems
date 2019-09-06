f9219a7bc751f5de60a3bae5c1a38e01c9189c1b
Tate Allen <tatea7@yahoo.com>
2019-09-06 11:57:32 -0400

    Added Engine files

---
+++ Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tate Allen on 9/6/19.
+//
+
+#include "Engine.h"
---
+++ Engine.h
@@ -0,0 +1,20 @@
+//
+// Created by Tate Allen on 9/6/19.
+//
+
+#ifndef L_SYSTEM_PARSER_ENGINE_H
+#define L_SYSTEM_PARSER_ENGINE_H
+
+namespace lsys {
+
+    class Engine {
+    public:
+        Engine();
+        ~Engine();
+
+    private:
+
+    };
+}
+
+#endif //L_SYSTEM_PARSER_ENGINE_H
